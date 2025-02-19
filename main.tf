6af0708a5580d89c5ce07f48a355ac7f54268805
Than Myint Tun <146816681+thanmyinttun01@users.noreply.github.com>
2025-02-19 14:37:06 +0630

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,14 +17,14 @@ module "eks" {
   cluster_name    = "tmt-poc-eks"
   cluster_version = "1.29"
 
-  vpc_id     = "vpc-07f5704ebee213784"
-  subnet_ids = ["subnet-028b31c3537c94b06", "subnet-01ee3661841f38898"]
+  vpc_id     = "vpc-0xxxxx"
+  subnet_ids = ["subnet-0xxxxx", "subnet-01xxxxxxx"]
 
   instance_type = "t3.medium"
   min_size      = 2
   max_size      = 3
   desired_size  = 2
-  ami_id        = "ami-039454f12c36e7620"
+  ami_id        = "ami-039454xxxxx"
   volume_size   = 200
 
   tags = {
